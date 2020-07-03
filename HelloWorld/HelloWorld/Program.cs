f4b4264896a6a129270e06539c0d2a9a3c317fae
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-07-03 13:38:23 +0300

    Add files via upload
    Verbatim strings and escape sequences

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,19 +11,11 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            //variables
-            string itemName = "Widget";
-            double pricePerOunce = 17.36;
-            string outputString;
-
-            //format output
-            //the {0:C} doesn't show the $ sign in front of pricePerOunce, instead, what it shows is "17,36 ?"
-            outputString = String.Format("{0,10},{1,10}", itemName, pricePerOunce);
-
-            //output to the user
-            //we might also input actual strings instead of variables
-            Console.WriteLine(String.Format("{0,10},{1,10}", "Item Name", "Price"));
-            Console.WriteLine(outputString);
+            //escape sequence by:
+            // 1. double \\
+            // 2. @ sign at the front @"C:\my documents\homedirectory\files"
+            string file = "C:\\my documents\\homedirectory\\files";
+            Console.WriteLine(file);
         }
     }
 }
