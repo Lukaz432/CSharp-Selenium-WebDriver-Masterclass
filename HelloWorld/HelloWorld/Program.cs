04cbe2e579a3c10b6490d806379a9c91fe0b3913
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-07-03 12:34:27 +0300

    Add files via upload
    basics

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //ask the user a question
+            Console.WriteLine("What is your favorite number?");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
