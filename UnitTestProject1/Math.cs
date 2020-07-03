4c09fb9bdff55fc3929d0bd4b5e06626d787a55c
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-07-03 16:01:48 +0300

    Add files via upload
    Math with integers

---
+++ UnitTestProject1/Math.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+        [TestClass]
+        public class IntegerOperations
+    {
+        //variables to hold two integer values
+        static int number1;
+        static int number2;
+
+
+        [ClassInitialize()]
+        public static void IntegerInitialize(TestContext testContext)
+        {
+            //set the values of the two numbers we'll be using
+            number1 = 10;
+            number2 = 5;
+        }
+
+        [TestMethod]
+        public void Test_Addition()
+        {
+            int sum = number1 + number2;
+            Console.WriteLine(sum);
+            Assert.AreEqual(sum, 15);
+
+        }
+
+        [TestMethod]
+        public void Test_Subtraction()
+        {
+            int difference = number1 - number2;
+            Assert.AreEqual(difference, 5);
+        }
+
+        [TestMethod]
+        public void Test_Multiplication()
+        {
+            int product = number1 * number2;
+            Assert.AreEqual(product, 50);
+        }
+
+        [TestMethod]
+        public void Test_Division()
+        {
+            int quotient = number1 / number2;
+            Assert.AreEqual(quotient, 2);
+        }
+
+        [TestMethod]
+        public void Test_Modulus()
+        {
+            int remainder = number1 % number2;
+            Assert.AreEqual(remainder, 0);
+        }
+
+    }
+}
